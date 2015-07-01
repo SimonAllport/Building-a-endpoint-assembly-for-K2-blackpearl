9ad548ea22cb7841687014dc4699549336d94220
SimonAllport <simon@greeneggs.co.uk>
2015-07-01 15:47:47 +0100

    Example code to be used with blog

---
+++ HelloWorld/MyHelloWorldClass.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    public class MyHelloWorldClass
+    {
+
+        public static string Hello()
+        {
+
+            return "Hello World";
+        }
+
+        public static string HelloWhoAreYou(string name)
+        {
+            return "Hello World " + name;
+        }
+
+    }
+}
